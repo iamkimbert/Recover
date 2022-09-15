25cd7796e35d082a58a66229a2ab72aca9a7178e
iamkimbert <iamkimbert@gmail.com>
2022-09-15 17:09:09 -0400

    Initial Commit

---
+++ Recover/RecoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoverApp.swift
+//  Recover
+//
+//  Created by Kimberly Townsend on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct RecoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
